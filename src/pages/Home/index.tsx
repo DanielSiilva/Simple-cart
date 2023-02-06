d865061f16072be4b5a836c2b3d76d5b0001a5e1
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-02-05 22:32:31 -0300

    Feat: adcionando função de adcionar ao carrinho

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,7 +7,8 @@ import {
 
 } from "./styled"
 
-import {Pizzas} from '../../data/data'
+import {Pizzas, CarItem} from '../../data/data'
+import { useState } from "react"
 
 
 
@@ -15,9 +16,44 @@ import {Pizzas} from '../../data/data'
 
 
 export function Home (){
-    
+    const [shoppingCart, setShoppingCart] = useState<CarItem[]>([])
 
 
+    //Função de adcionar item
+
+    function handleAddToCart(id: number){
+        //Pegar o id do produto
+        const idProduct = Pizzas.find((pizza) => pizza.id === id)
+
+        //Verificando se já está no carrinho 
+        const alreadyInShoppingCart = shoppingCart.find((item) => item.product.id === id)
+
+        //se o livro estiver no carrinho de compras
+
+        if(alreadyInShoppingCart){
+            const newShoppingCart:CarItem[] = shoppingCart.map((item) =>{
+                if(item.product.id === id)
+                ({
+                    ...item,
+                    quantity: item.quantity++
+                });
+                return item  
+            });
+            setShoppingCart(newShoppingCart)
+            return
+        }
+
+        // se o livro ainda não estiver no carrinho de compras
+
+        const cartItem: CarItem = {
+            product: idProduct!,
+            quantity: 1,
+        }
+
+        const newShoppingcart: CarItem[] = [...shoppingCart, cartItem]
+
+        setShoppingCart(newShoppingcart)
+    }
 
 
 
@@ -40,7 +76,7 @@ export function Home (){
                             <img  src={item.image}/>
                             <p>{item.price}</p>
 
-                            <button>Adcionar</button>
+                            <button onClick={()=>handleAddToCart(item.id)}>Adcionar</button>
                         </Product>
                     )
                 })}
@@ -48,7 +84,16 @@ export function Home (){
             
             <Cart>
                 <h1>Seu Carrinho</h1>
-                
+                {shoppingCart.map((item)=>{
+                    return(
+                        <div key={item.product.id}>
+                            <p>{item.product.title}</p>
+                            <img src={item.product.image} />
+                            <p>{item.product.price}</p>
+                            <p>{item.quantity}</p>
+                        </div>
+                    )
+                })}
             </Cart>
         </Wrapper>
     )
